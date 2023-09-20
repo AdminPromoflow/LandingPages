8ef79a298475340a203c4c536f4aa49cd28bf997
aelnajard <aelnajard@gmail.com>
2023-09-20 17:37:16 -0500

    “…”

---
+++ General/Menu/Menu.php
@@ -0,0 +1,84 @@
+<style media="screen">
+  .menu{
+    position: sticky;
+    top: 0px;
+    left: 0px;
+    height: 6vw;
+    min-height: 60px;
+    max-height: 100px;
+    width: 100%;
+    background-color: rgba(255,255,255,.3);
+    z-index: 10;
+    box-shadow: 12px 0px 15px rgba(0,0,0,.3);
+    font-family: 'IBM Plex Sans Condensed', sans-serif;
+  }
+  .logo{
+    position: absolute;
+    height: 100%;
+    width: 25%;
+    left: 1vw;
+  }
+  .containerItems{
+    position: absolute;
+    height: 100%;
+    width: 50%;
+    left: calc(2vw + 25%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .containerItems h3{
+    position: relative;
+    display: inline-block;
+    margin: 0px;
+    margin: 0;
+    font-weight: 400;
+    cursor: pointer;
+    border-bottom: 2px solid transparent;
+    padding: 0 8px;
+    border-left: 2px solid black;
+
+    text-shadow: 1px 1px 2px rgba(34, 61, 67, .3);
+    transition: .5s;
+
+  }
+  .transparentLeftLine{
+    border-left: 2px solid transparent!important;
+
+  }
+  .containerItems h3:hover{
+    text-shadow: 1px 1px 2px rgba(34, 61, 67, 1);
+  }
+  .containerItems2{
+    position: absolute;
+    height: 100%;
+    width: calc(25% - 4vw);
+    left: calc(3vw + 75%);
+    background-color: orange;
+  }
+  .logo img{
+    position: relative;
+    left: 0px;
+    top: 50%;
+    transform: translateY(-50%);
+    filter: drop-shadow(1px 1px 1px rgba(0,0,0,.9));
+  }
+</style>
+<section class="menu">
+  <div class="logo">
+    <img src="../General/Menu/img/Logo.png" alt="">
+  </div>
+  <div class="containerItems">
+    <h3 class="transparentLeftLine">Home </h3>
+    <h3>Products </h3>
+    <h3>My Lanyards </h3>
+    <h3>About us </h3>
+    <h3>Contact us</h3>
+
+  </div>
+  <div class="containerItems2">
+  </div>
+</section>
+<script type="text/javascript">
+
+</script>
--- Index/index.php
+++ Index/index.php
@@ -3,62 +3,67 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,200&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
     <title></title>
   </head>
   <body>
     <style media="screen">
-    body{
-      margin: 0px;
-      padding: 0px;
-      background: #051527;
-    }
-    .background{
-      position: relative;
-      background-image: url('../Index/img/background.png');
-      position: relative;
-      height: 120vh;
-      width: auto;
-      background-size: cover;
+      body{
+        margin: 0px;
+        padding: 0px;
+        background: #051527;
+      }
+      .background{
+        position: relative;
+        background-image: url('../Index/img/background.png');
+        position: relative;
+        height: 120vh;
+        width: auto;
+        background-size: cover;
+        filter: brightness(1.7);
 
-      background-color: #cccccc; /* Used if the image is unavailable */
-      background-position: center; /* Center the image */
-      background-repeat: no-repeat; /* Do not repeat the image */
-      padding: 3vw;
+        background-color: #cccccc; /* Used if the image is unavailable */
+        background-position: center; /* Center the image */
+        background-repeat: no-repeat; /* Do not repeat the image */
+        padding: 3vw;
 
-      display: flex;
-      justify-content: center;
-      flex-wrap: wrap;
-      border-radius: 10px;
-      box-shadow: 30px 30px  40px black;
-
-    }
-    .container{
-      position: relative;
-      height: 90%;
-      width: 80vw;
-      background: #0A1118;
-      overflow-y: scroll;
-        }
-    .content{
-      position: relative;
-      height: 2000px;
-
-    }
-    .content img{
-      position: relative;
-      width: 100%;
-
-    }
-
-
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+      }
+      .container{
+        position: relative;
+        height: 90%;
+        width: 80vw;
+        background: rgb(8,12,20);
+        background: linear-gradient(90deg, rgba(8,12,20,1) 0%, rgba(78,121,116,1) 49%, rgba(44,71,76,1) 100%);
+        overflow-y: scroll;
+        box-shadow: 30px 30px  40px black;
+        border-radius: 10px;
+        box-shadow: 30px 30px  40px rgba(20,20,20,.5), -30px -30px  40px rgba(30,30,30,.5);
+        filter: brightness(0.6);
+        min-width: 300px;
+          }
+      .content{
+        position: relative;
+        height: 2000px;
+      }
+      .content img{
+        position: relative;
+        width: 100%;
+      }
     </style>
     <section>
       <div class="background">
         <div class="container">
           <div class="content">
+            <?php include "../General/Menu/Menu.php" ?>
             <img src="../Index/img/Example.png" alt="">
           </div>
-
         </div>
       </div>
     </section>

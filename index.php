0aef35b0915599ae4966649d031541638b70500f
aelnajard <aelnajard@gmail.com>
2023-09-20 14:11:38 -0500

    “…”

--- index.php
+++ index.php
@@ -2,65 +2,11 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
   </head>
   <body>
-    <style media="screen">
-    body{
-      margin: 0px;
-      padding: 0px;
-    }
-    .background{
-      position: relative;
-      background-image: url('img/background.png');
-      position: relative;
-      height: 120vh;
-      width: auto;
-      background-size: cover;
-
-      background-color: #cccccc; /* Used if the image is unavailable */
-      background-position: center; /* Center the image */
-      background-repeat: no-repeat; /* Do not repeat the image */
-      padding: 3vw;
-
-      display: flex;
-      justify-content: center;
-      flex-wrap: wrap;
-
-    }
-    .container{
-      position: relative;
-      height: 90%;
-      width: 80vw;
-      background: green;
-      overflow-y: scroll;
-        }
-    .content{
-      position: relative;
-      height: 2000px;
-
-    }
-    .content img{
-      position: relative;
-      width: 100%;
-      
-    }
-
-
-    </style>
-    <section>
-      <div class="background">
-        <div class="container">
-          <div class="content">
-            <img src="img/Example.png" alt="">
-          </div>
-
-        </div>
-      </div>
-    </section>
     <script type="text/javascript">
-
+      window.open("Index/index.php", "_self");
     </script>
   </body>
 </html>

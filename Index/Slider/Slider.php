ba700fbbfb13e9d5c113f7b07298c03d8497bb78
aelnajard <aelnajard@gmail.com>
2023-09-21 18:29:06 -0500

    “…”

--- Index/Slider/Slider.php
+++ Index/Slider/Slider.php
@@ -1,48 +1,45 @@
 <style media="screen">
-  .slider-container {
-      width: 300%;
-      overflow: hidden;
-  }
+.slider-container {
+    width: 100%;
+    overflow: hidden;
+}
 
-  .slider {
-      display: flex;
-      transition: transform 1s ease-in-out;
-  }
+.slider {
+    display: flex;
+    transition: transform 1s ease-in-out;
+}
 
-  .slider img {
-      width: 33.333%;
-      height: auto;
-  }
+.slider img {
+    width: 100vw;
+    height: auto;
+}
 
-  .slider-buttons {
-      display: flex;
-      justify-content: center;
-      margin-top: 0px;
-  }
+.slider-buttons {
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+}
 
-  .slider-buttons button {
-      margin: 0 10px;
-      padding: 5px 10px;
-      background-color: #333;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-  }
-  .darker{
-    filter: brightness(.8);
-  }
+.slider-buttons button {
+    margin: 0 10px;
+    padding: 5px 10px;
+    background-color: #333;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
 </style>
 <div class="slider-container">
-    <div class="slider">
-        <img class="darker" src="../Index/Slider/img/imagen1.png" alt="Imagen 1">
-        <img src="../Index/Slider/img/imagen2.png" alt="Imagen 2">
-        <img src="../Index/Slider/img/imagen4.png" alt="Imagen 3">
-    </div>
-    <div class="slider-buttons">
-        <button onclick="previousSlide()">Anterior</button>
-        <button onclick="nextSlide()">Siguiente</button>
-    </div>
-</div>
+       <div class="slider">
+           <img src="imagen1.jpg" alt="Imagen 1">
+           <img src="imagen2.jpg" alt="Imagen 2">
+           <img src="imagen3.jpg" alt="Imagen 3">
+       </div>
+       <div class="slider-buttons">
+           <button onclick="previousSlide()">Anterior</button>
+           <button onclick="nextSlide()">Siguiente</button>
+       </div>
+   </div>
 <script type="text/javascript">
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.slider img');
@@ -50,21 +47,21 @@ const imageWidth = images[0].clientWidth;
 let currentSlide = 0;
 
 function slideTo(index) {
-slider.style.transform = `translateX(-${index * imageWidth}px)`;
-currentSlide = index;
+    slider.style.transform = `translateX(-${index * imageWidth}px)`;
+    currentSlide = index;
 }
 
 function previousSlide() {
-currentSlide = (currentSlide - 1 + images.length) % images.length;
-slideTo(currentSlide);
+    currentSlide = (currentSlide - 1 + images.length) % images.length;
+    slideTo(currentSlide);
 }
 
 function nextSlide() {
-currentSlide = (currentSlide + 1) % images.length;
-slideTo(currentSlide);
+    currentSlide = (currentSlide + 1) % images.length;
+    slideTo(currentSlide);
 }
 
-setInterval(nextSlide, 6000); // Cambia de imagen cada 3 segundos
+setInterval(nextSlide, 3000); // Cambia de imagen cada 3 segundos
 
 
 

08ee8ccd60a434cfd71b28bb0525b07778cc2cc1
aelnajard <aelnajard@gmail.com>
2023-11-12 12:15:45 -0500

    “…”

--- public/js/global/menu.js
+++ public/js/global/menu.js
@@ -1,85 +1,67 @@
-// Define a Menu class to handle menu interactions
 class Menu {
   constructor() {
-    // Add click events to open the login section
     for (let i = 0; i < openLoginButtons.length; i++) {
       openLoginButtons[i].addEventListener("click", function() {
-        // Call the openLogin method from login Class
         loginClass.openLogin();
         registerClass.openRegister();
       });
     }
 
-
-    // Define the URL and the JSON data you want to send
-    const url = "../../controller/users/session-login.php"; // Replace with your API endpoint URL
+    const url = "../../controller/users/session-login.php";
     const data = {
       action: "checkSessionLogin"
     };
     this.makeAjaxRequestCheckSessionLogin(url, data);
 
-    // Add click event to open the mobile menu
     openMenuMobileButton.addEventListener("click", this.openMenuMobile.bind(this));
-
-    // Add click event to close the mobile menu
     closeMenuMobileButton.addEventListener("click", this.closeMenuMobile.bind(this));
-
-    // Add a global click event listener to close the mobile menu when clicking outside
     document.addEventListener("click", this.handleClickOutside.bind(this));
   }
 
-  // Method to open the mobile menu
   openMenuMobile() {
     closeMenuMobile.style.display = "block";
     menuMobile.style.left = "calc(100% - 300px)";
     openMenuMobile.style.display = "none";
   }
 
-  // Method to close the mobile menu
   closeMenuMobile() {
     closeMenuMobile.style.display = "none";
     menuMobile.style.left = "calc(100%)";
     openMenuMobile.style.display = "block";
   }
 
-  // Method to handle clicks outside the menu
   handleClickOutside(event) {
     if (!openMenuMobile.contains(event.target)) {
       if (!menuMobile.contains(event.target)) {
-        // Call your function or perform the desired action here
         this.closeMenuMobile();
       }
     }
   }
+
   makeAjaxRequestCheckSessionLogin(url, data){
-    // Make the request using the Fetch API
     fetch(url, {
-      method: "POST", // HTTP POST method to send data
+      method: "POST",
       headers: {
-        "Content-Type": "application/json" // Indicate that you're sending JSON
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify(data) // Convert the JSON object to a JSON string and send it
+      body: JSON.stringify(data)
     })
       .then(response => {
         if (response.ok) {
-          return response.text(); // or response.json() if you expect a JSON response
+          return response.text();
         }
         throw new Error("Network error.");
       })
       .then(data => {
         data = JSON.parse(data);
         this.showItemsLoginMenu(data["message"]);
-
-
-        // The code inside this function will run when the request is complete
-        // Hide the register form with a sliding animation
       })
       .catch(error => {
         console.error("Error:", error);
       });
   }
+
   showItemsLoginMenu(action){
-    //alert(action);
     if (action) {
       for (var i = 0; i < showItemsMenuLoginFalse.length; i++) {
         showItemsMenuLoginFalse[i].style.display = "none";
@@ -96,20 +78,15 @@ class Menu {
         showItemsMenuLoginTrue[i].style.display = "none";
       }
     }
-
   }
-
 }
-// Get elements and buttons related to the menu
-const openLoginButtons = document.querySelectorAll('.openLogin'); // Buttons to open the login
-const openMenuMobileButton = document.getElementById("openMenuMobile"); // Button to open mobile menu
-const closeMenuMobileButton = document.getElementById("closeMenuMobile"); // Button to close mobile menu
-const menuMobile = document.getElementById("menuMobile"); // Mobile menu container
+
+const openLoginButtons = document.querySelectorAll('.openLogin');
+const openMenuMobileButton = document.getElementById("openMenuMobile");
+const closeMenuMobileButton = document.getElementById("closeMenuMobile");
+const menuMobile = document.getElementById("menuMobile");
 
 const showItemsMenuLoginFalse = document.querySelectorAll(".showItemsMenuLoginFalse");
-
 const showItemsMenuLoginTrue = document.querySelectorAll(".showItemsMenuLoginTrue");
 
-
-// Create an instance of the Menu class
 const menuClass = new Menu();

d7d8a778fe400f7e0124d7ea0b41268611dc6e97
aelnajard <aelnajard@gmail.com>
2023-11-11 19:11:43 -0500

    “…”

--- views/global/menu.php
+++ views/global/menu.php
@@ -6,13 +6,15 @@
   <div class="containerItems">
     <h3 class="transparentLeftLine">Home </h3>
     <h3>Products </h3>
-  <!--  <h3>My Lanyards </h3>-->
+    <h3 class="showItemsMenuLoginTrue" >My Lanyards </h3>
     <h3>About us </h3>
     <h3>Contact us</h3>
   </div>
   <div class="containerItems2">
-    <img src="../../public/img/global/menu/checkout.png" alt="">
-    <h3  class="openLogin">Login</h3>
+    <img class="showItemsMenuLoginTrue"  src="../../public/img/global/menu/checkout.png" alt="">
+    <h3  class="openLogin showItemsMenuLoginFalse">Register</h3>
+    <h3  class="openLogin showItemsMenuLoginFalse">Login</h3>
+    <h3  class="openLogin showItemsMenuLoginTrue">Login out</h3>
 
   </div>
   <div class="containerItems3">
@@ -24,7 +26,7 @@
   <section id="menuMobile" class="menuMobile">
     <h3>Home </h3>
     <h3>Products </h3>
-  <!--  <h3>My Lanyards </h3> -->
+    <h3>My Lanyards </h3>
     <h3>About us </h3>
     <h3 >Contact us</h3>
     <h3 class="openLogin">Login</h3>

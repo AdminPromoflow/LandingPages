6f7543f028b885d62544f85e4b286cd52cd105a3
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2024-01-16 12:57:54 -0500

    d

--- views/global/customize-lanyard/sections/price.php
+++ views/global/customize-lanyard/sections/price.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="../assets/css/global/customize-lanyard/sections/price.css">
 <section class="price">
-  <h1>Hola</h1>
+  <h3>Cost per Lanyard</h3>
+  <h3>17.500£</h3>
 </section>
 <script src="../assets/js/customize-lanyard/sections/price.js"></script>
